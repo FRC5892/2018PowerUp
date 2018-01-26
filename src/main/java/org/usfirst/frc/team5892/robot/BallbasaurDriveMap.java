4ea583391781523a813237c9e805c1d489bfaa71
Kai Page <kaibug@gmail.com>
2018-01-26 17:29:27 -0600

    Slight optimization

--- src/main/java/org/usfirst/frc/team5892/robot/BallbasaurDriveMap.java
+++ src/main/java/org/usfirst/frc/team5892/robot/BallbasaurDriveMap.java
@@ -2,10 +2,7 @@ package org.usfirst.frc.team5892.robot;
 
 public class BallbasaurDriveMap extends RobotMap {
     public BallbasaurDriveMap() {
-        MotorInfo[] _leftDrive = {new MotorInfo(7, true), new MotorInfo(3, true)};
-        leftDrive = _leftDrive;
-
-        MotorInfo[] _rightDrive = {new MotorInfo(8, false), new MotorInfo(2, false)};
-        rightDrive = _rightDrive;
+        leftDrive = new MotorInfo[]{new MotorInfo(7, true), new MotorInfo(3, true)};
+        rightDrive = new MotorInfo[]{new MotorInfo(8, false), new MotorInfo(2, false)};
     }
 }
