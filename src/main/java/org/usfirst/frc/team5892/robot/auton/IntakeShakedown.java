50b4a094411bd3e51abecb9e99c9430097acde36
Kai Page <kaibug@gmail.com>
2018-03-13 12:52:01 -0500

    Better shakedown

--- src/main/java/org/usfirst/frc/team5892/robot/auton/IntakeShakedown.java
+++ src/main/java/org/usfirst/frc/team5892/robot/auton/IntakeShakedown.java
@@ -1,13 +1,12 @@
 package org.usfirst.frc.team5892.robot.auton;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
-import org.usfirst.frc.team5892.robot.MathUtils;
+import org.usfirst.frc.team5892.robot.subsystems.elevator.RunElevator;
 import org.usfirst.frc.team5892.robot.subsystems.intake.StartIntaking;
 
 public class IntakeShakedown extends CommandGroup {
     public IntakeShakedown() {
         addParallel(new StartIntaking());
-        addSequential(new AutoStraightDrive(1, 0, MathUtils.encoderInches(3)));
-        addSequential(new AutoStraightDrive(-1, 0, MathUtils.encoderInches(3)));
+        addSequential(new RunElevator(1), 0.125);
     }
 }
