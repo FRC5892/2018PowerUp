2c6f1bd980926e9e10de0e51f08167fd2a79bfed
Kai Page <kaibug@gmail.com>
2018-03-13 11:32:27 -0500

    Trying out new way of doing angles

--- src/main/java/org/usfirst/frc/team5892/robot/auton/IntakeShakedown.java
+++ src/main/java/org/usfirst/frc/team5892/robot/auton/IntakeShakedown.java
@@ -7,7 +7,7 @@ import org.usfirst.frc.team5892.robot.subsystems.intake.StartIntaking;
 public class IntakeShakedown extends CommandGroup {
     public IntakeShakedown() {
         addParallel(new StartIntaking());
-        addSequential(new AutoStraightDrive(1, 0, MathUtils.encoderInches(6)));
-        addSequential(new AutoStraightDrive(-1, 0, MathUtils.encoderInches(6)));
+        addSequential(new AutoStraightDrive(1, 0, MathUtils.encoderInches(3)));
+        addSequential(new AutoStraightDrive(-1, 0, MathUtils.encoderInches(3)));
     }
 }
