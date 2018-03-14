e7feb555e44c94734260cbd7f6c37be1f5cc83cb
Kai Page <kaibug@gmail.com>
2018-03-14 13:34:30 -0500

    More auton

--- src/main/java/org/usfirst/frc/team5892/robot/auton/ScoreToScaleAuto.java
+++ src/main/java/org/usfirst/frc/team5892/robot/auton/ScoreToScaleAuto.java
@@ -22,30 +22,33 @@ public class ScoreToScaleAuto extends DynamicAuton {
             //addSequential(new IntakeShakedown());
             if (pos == gameData.charAt(0)) {
 
-                addSequential(new AutoStraightDrive(0.9, 0, encoderInches(220)));
+                addSequential(new AutoStraightDrive(1, 0, encoderInches(120)));
+                addSequential(new AutoStraightDrive(0.8, 0, encoderInches(100)));
                 addSequential(new AutoGyroRotate(45 * turnDir));
                 //addSequential(new RunElevator(0.8), 2);
-                //addSequential(new OuttakeCommand(), 0.5);
+                addSequential(new OuttakeCommand(), 0.5);
                 //addParallel(new RunElevator(-0.8), 1);
                 addSequential(new AutoGyroRotate(145 * turnDir));
                 addSequential(new StartIntaking());
-                addSequential(new AutoStraightDrive(0.8, 145 * turnDir, encoderInches(24)));
-                addSequential(new WaitCommand(), 0.5);
-                addSequential(new WaitCommand(Robot.intake::isButtonPressed), 0.5);
-                addSequential(new AutoGyroRotate(10 * turnDir));
-                addSequential(new AutoStraightDrive(0.8, 10 * turnDir, encoderInches(12)));
+                addParallel(new AutoStraightDrive(0.6, 145 * turnDir, encoderInches(40)));
+                addSequential(new WaitCommand(Robot.intake::isButtonPressed), 2);
+                addSequential(new AutoStraightDrive(-0.7, 145 * turnDir, encoderInches(6)));
+                addSequential(new AutoGyroRotate(15 * turnDir));
+                addSequential(new AutoStraightDrive(0.8, 15 * turnDir, encoderInches(4)));
                 //addSequential(new RunElevator(0.8), 2);
                 addSequential(new OuttakeCommand(), 0.5);
 
             } else {
 
-                addSequential(new AutoStraightDrive(0.8, 0, encoderInches(190)));
+                addSequential(new AutoStraightDrive(0.8, 0, encoderInches(180)));
+                addSequential(new WaitCommand(0.5));
                 addSequential(new AutoGyroRotate(90 * turnDir));
-                addSequential(new AutoStraightDrive(0.8, 90 * turnDir, encoderInches(171)));
+                addSequential(new AutoStraightDrive(0.8, 90 * turnDir, encoderInches(175)));
+                addSequential(new WaitCommand(0.5));
                 addSequential(new AutoGyroRotate(0));
                 addSequential(new AutoStraightDrive(0.6, 0, encoderInches(10)));
                 //addSequential(new RunElevator(0.5), 4);
-                //addSequential(new OuttakeCommand(), 1);
+                addSequential(new OuttakeCommand(), 1);
 
             }
         }
